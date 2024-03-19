2862cdfaecc7983467bf37304247f0e1476aa8ea
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-19 10:04:48 -0300

    Index routes.

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,20 @@ import Expenses from "./routs/Expenses/index.tsx";
 import Invoices from "./routs/Invoices/index.tsx";
 import NotFound from "./routs/NotFound/index.tsx";
 import Invoice from "./routs/Invoices/Invoice/index.tsx";
+import InvoicesIndex from "./routs/Invoices/InvoicesIndex/index.tsx";
+import Welcome from "./routs/Welcome/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} >
+      <Route path="/" element={<App />}>
+        <Route index element={<Welcome/>}/>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} >
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex/>}/>
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
-        <Route path="*" element = {<NotFound/>}
-        />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>
